ce294725d0b85f68b2f6a240e244b169b60ba50d
Bata Zoaka <102160639+Ella-1@users.noreply.github.com>
2026-02-14 22:45:35 +0100

    fixing chaka UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Provider } from "@/components/ui/provider"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+       <Provider>{children}</Provider> 
       </body>
     </html>
   );
